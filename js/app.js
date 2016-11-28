f288de687f26f694bd1b949f2877fbba1f929268
Baptiste, Nicolas (ESI) <nicolas.baptiste@adp.com>
2016-11-28 18:07:01 +0100

    WIP

--- js/app.js
+++ js/app.js
@@ -1,13 +1,57 @@
 (function() {
+
   angular.module('poc', ['ngRoute'])
     .config(function($routeProvider, $locationProvider) {
       $routeProvider
         .when('/', {
-          templateUrl: 'home.html'
+          templateUrl: 'home.html',
+          controller: 'HomeController',
+          controllerAs: 'ctrl'
         })
         .when('/article/:id', {
           templateUrl: 'article.html'
         });
       $locationProvider.html5Mode(true);
     });
+
+  angular.module('poc')
+    .controller('HomeController', ['prismicService', '$scope', function (prismicService, $scope) {
+        var vm = this;
+        prismicService.getArticles().then(articles => {
+          vm.articles = articles.map(article => ({
+            body: article.getStructuredText('article.body').getFirstParagraph().text,
+            title: article.getText('article.title'),
+            image: article.getImage('article.picture').url
+          }));
+        });   
+    }]);
+
+  angular.module('poc')
+    .provider('prismicService', function() {
+      var self = this;
+
+      this.apiUrl = 'https://blogpoc.prismic.io/api'
+      
+    
+      this.$get = Service;
+
+      function Service ($q) {
+        var apiPromise = Prismic.api(self.apiUrl); 
+
+        return {
+          getArticles: () => 
+            {
+              var deferred = $q.defer();
+
+              apiPromise
+                .then(api => api.query(Prismic.Predicates.at('document.type', 'article')))
+                .then(response => deferred.resolve(response.results));
+
+              return deferred.promise;
+            }
+        }
+
+      }
+    })
+
 })();
